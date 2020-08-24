be867fd01de3c4ca65b4a2c3af5051f6a0a5028b
Joe Frazier <frazierj@wustl.edu>
2020-08-24 16:19:30 -0500

    Header

--- README.md
+++ README.md
@@ -42,3 +42,10 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+
+
+## Pushing to gh-pages
+git add dist && git commit -m "Initial dist subtree commit"
+
+git subtree push --prefix dist origin gh-pages
--- src/components/dashboard/home/Home.tsx
+++ src/components/dashboard/home/Home.tsx
@@ -1,20 +1,71 @@
-import * as React from 'react';
-import "./Home.scss";
-const Home = () => {
-    return(
-        <div className="text-primary text-center">
-            <div className="vh-100 bg-gradient-light">
-                Hello
-            </div>
-            <div className="vh-100 bg-primary">
-                Hello
-            </div>
-            <div className="vh-100 bg-secondary">
-                Hello
-            </div>
+import * as React from "react";
 
-        
-        </div>
-    );
+import CardDeck from "react-bootstrap/CardDeck";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import BlogCard from "../blog/BlogCard";
+
+import Header from '../../shared/Header';
+
+import background1 from "../../../assets/background1.gif";
+import wave from "../../../assets/wave.webp";
+import "./Home.scss";
+
+const Blogs = [
+  {
+    title: "Test",
+    author: "Joe Frazier",
+    blurb: "This is a test post",
+    content:
+      "THis is dafsf asdg adsf g asd v asdjvbalsdbvas dasdblvkasjdb faksdjf kjbvasdlbaliurgasjdb",
+    image: background1,
+    posted: Date.now(),
+    lastUpdate: Date.now(),
+  },
+  {
+    title: "Test2",
+    author: "Joe Frazier",
+    blurb: "This is a test post",
+    content:
+      "THis is dafsf asdg adsf g asd v asdjvbalsdbvas dasdblvkasjdb faksdjf kjbvasdlbaliurgasjdb",
+    image: background1,
+    posted: Date.now(),
+    lastUpdate: Date.now(),
+  },
+  {
+    title: "Test3",
+    author: "Joe Frazier",
+    blurb: "This is a test post",
+    content:
+      "THis is dafsf asdg adsf g asd v asdjvbalsdbvas dasdblvkasjdb faksdjf kjbvasdlbaliurgasjdb",
+    image: background1,
+    posted: Date.now(),
+    lastUpdate: Date.now(),
+  },
+];
+
+const Home = () => {
+  return (
+    <div className="text-text">
+      <Header />
+      <Container fluid className="bg-white d-flex flex-column py-5">
+        <Container className="bg-light d-flex flex-column py-5 rounded-lg">
+          <h1 className="text-center pb-5">Blog</h1>
+
+          <Row className="px-5 my-2">
+            {Blogs.map((blog) => (
+              <Col xs={12} md={6} xl={4} className="px-2 py-3">
+                <BlogCard blog={blog} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </Container>
+    </div>
+  );
 };
 export default Home;
